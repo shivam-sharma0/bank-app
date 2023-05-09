4232ae7eabb3412318022d9dd8d7ae90b0c979a0
shivam-sharma0 <vashistshivam931@gmail.com>
2023-05-09 22:44:09 +0530

    last commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,11 @@
 public interface BankAccount {
+ Boolean withdrwaMoney(int money);
 
+ Boolean addMoney(int money);
+
+ int checkBalnace();
+
+ int getRDI();
+
+ int getTotalInterest(int amount,int year);
 }
--- src/HdfcBankAccount.java
+++ src/HdfcBankAccount.java
@@ -1,2 +1,51 @@
-public class HdfcBankAccount {
+import java.util.UUID;
+
+public class HdfcBankAccount implements BankAccount{
+
+    private  String accountNumber;
+    private  int balance;
+    private int roi;
+
+    public HdfcBankAccount(int balance) {
+        this.balance = balance;
+        this.accountNumber= String.valueOf(UUID.randomUUID());
+        this.roi=5;
+    }
+
+
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+
+    @Override
+    public Boolean withdrwaMoney(int money) {
+        return null;
+    }
+
+    @Override
+    public Boolean addMoney(int money) {
+        return null;
+    }
+
+    @Override
+    public int checkBalnace() {
+        return this.balance;
+    }
+
+    @Override
+    public int getRDI() {
+        return this.roi;
+    }
+
+    @Override
+    public int getTotalInterest(int amount, int year) {
+        return amount*year/roi;
+    }
+
 }
